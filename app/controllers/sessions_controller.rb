1a546c4f4af261891b4479af900ae2ea79e8b696
Moin <kmoin786@gmail.com>
2020-06-15 04:13:47 +0530

    add edge cases for session and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    @user = User.where(name: user_params[:name]).first
+    @user = User.where(name: user_params[:name].downcase).first
     if @user
       session[:user_id] = @user.id
-      flash[:notice] = "Successfully logged in as #{@user.name}"
+      flash[:notice] = "Successfully logged in as #{@user.name.capitalize}"
       redirect_to root_path
     else
       flash[:alert] = "Name does not exist! Please try again or sign up"
@@ -13,9 +13,14 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-    session[:user_id] = nil
-    flash[:notice] = "Successfully logged out!"
-    redirect_to root_path
+    if logged_in?
+      session[:user_id] = nil
+      flash[:notice] = "Successfully logged out!"
+      redirect_to root_path
+    else
+      flash[:alert] = "User not logged in!"
+      redirect_to root_path
+    end
   end
   def user_params
     params.require(:user).permit(:name)
