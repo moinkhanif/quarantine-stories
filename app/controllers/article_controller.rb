9b1ba36646d27675cde705fdcf6ac80823537c86
Moin <kmoin786@gmail.com>
2020-06-18 01:44:40 +0530

    fix linter errors

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,15 +1,14 @@
 class ArticleController < ApplicationController
-  before_action :login_required, only: %i[new create  destroy]
-  def new
-    
-  end
+  before_action :login_required, only: %i[new create destroy]
+  def new; end
+
   def create
     @article = Article.new(article_params)
     @article[:user_id] = current_user.id
     if @article.save
       flash[:notice] = 'Article created successfully!'
     else
-      flash[:alert] = "Failed to create article please try again!"
+      flash[:alert] = 'Failed to create article please try again!'
     end
     redirect_to root_path
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @categories = Category.includes(:articles).order(priority: :desc)
-    @featured_article = Article.find(Vote.group(:article_id).count.max_by { |_a, v| v }.first) if !Vote.all.empty?
+    @featured_article = Article.find(Vote.group(:article_id).count.max_by { |_a, v| v }.first) unless Vote.all.empty?
   end
 end
