ba3c34cb20ba8d433e841530d3982cd7d5506743
Moin Khan <kmoin786@gmail.com>
2020-06-24 22:34:05 +0530

    fix linter errors

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,24 +1,24 @@
 module HomeHelper
   def front_home
     if !@categories.empty? and @featured_article
-      content_tag(:article, style: "background: linear-gradient(to bottom,rgba(0, 0, 0, 0.25), rgb(0, 0, 0, 0.9) 100%), url(#{@featured_article.image.exists? ? @featured_article.image.url : 'https://source.unsplash.com/random'}) no-repeat center;") do
-        content_tag(:h2,class: "article-title") do
+      content_tag(:article, style: article_background_styles(@featured_article)) do
+        content_tag(:h2, class: 'article-title') do
           @featured_article.title
         end
       end
     else
-      content_tag(:article, style: "background: linear-gradient(to bottom,rgba(0, 0, 0, 0.25), rgb(0, 0, 0, 0.9) 100%), url('https://source.unsplash.com/random') no-repeat center;"
-         ) do
-        content_tag(:h2, class: "article-title") do
-          "No articles wth votes found"
+      content_tag(:article, style: article_background_styles) do
+        content_tag(:h2, class: 'article-title') do
+          'No articles wth votes found'
         end
       end
     end
   end
+
   def home_categories
     @categories.map do |category|
-      article = category.articles.sort_by(&:created_at).last
-      content_tag(:article, style:"background: linear-gradient(to bottom,rgba(0, 0, 0, 0.25), rgb(0, 0, 0, 0.9) 100%), url(#{(article and article.image.exists?) ? article.image.url : "https://source.unsplash.com/random" }) no-repeat center;") do
+      article = category.articles.max_by(&:created_at)
+      content_tag(:article, style: article_background_styles(article)) do
         concat(content_tag(:h2) do
           link_to category.name, category_path(category)
         end)
@@ -29,5 +29,18 @@ module HomeHelper
         end
       end
     end.join.html_safe
+  end
+
+  def article_background_styles(article = nil)
+    style = 'background: linear-gradient(to bottom,rgba(0, 0, 0, 0.25), rgb(0, 0, 0, 0.9) 100%), '
+    style + if article
+              "url(#{article_images(article)}) no-repeat center;"
+            else
+              "url('https://source.unsplash.com/random') no-repeat center;"
+            end
+  end
+
+  def article_images(article)
+    article.image.exists? ? article.image.url : 'https://source.unsplash.com/random'
   end
 end
